2e9779675324353681d5e74ad1cc0a621b85f965
fredsamuel <fredsamuel403@gmail.com>
2025-03-14 10:00:00 +0500

    project setup Final

---
+++ config/configs.js
@@ -0,0 +1,13 @@
+// Set the connection string based from the config vars of the production server
+// To run locally use 'mongodb://localhost/mern-crud' instead of process.env.DB
+
+module.exports = {
+  db: 
+  'mongodb+srv://zainjanjua:2cG8xCeRzudPuRqn@nodeappfordatadog.aiq7rql.mongodb.net/'
+};
+
+
+// module.exports = {
+//   db: 
+//   'mongodb+srv://fahadtest:4GcqNZtJMBVu9xv6@fahad-test-clu.gcd2o.mongodb.net/merndb?retryWrites=true&w=majority&appName=fahad-test-clu'
+// };
